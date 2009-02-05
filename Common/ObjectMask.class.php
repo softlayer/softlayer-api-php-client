fb0ba5f0d535cbf966e11487be8dc70bd10fc754
Kevin Laude <klaude@softlayer.com>
2009-02-04 21:13:03 -0600

    Shoring up documentation and the license for release under the SLDN user.

--- Common/ObjectMask.class.php
+++ Common/ObjectMask.class.php
@@ -1,4 +1,31 @@
 <?php
+/**
+ * Copyright (c) 2009, SoftLayer Technologies, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither SoftLayer Technologies, Inc. nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /**
  * A simple object mask implementation.
@@ -29,10 +56,17 @@
  * For more on object mask usage in the SoftLayer API please see
  * http://sldn.softlayer.com/wiki/index.php/Using_Object_Masks_in_the_SoftLayer_API .
  *
+ * The most up to date version of this library can be found on the SoftLayer
+ * github public repositories: http://github.com/softlayer/ . Please post to
+ * the SoftLayer forums <http://forums.softlayer.com/> or open a support ticket
+ * in the SoftLayer customer portal if you have any questions regarding use of
+ * this library.
+ *
  * @author      SoftLayer Technologies, Inc. <sldn@softlayer.com>
  * @copyright   Copyright (c) 2008, Softlayer Technologies, Inc
- * @license     http://creativecommons.org/licenses/by/3.0/us/ Creative Commons Attribution 3.0 US
+ * @license     http://sldn.softlayer.com/wiki/index.php/License
  * @see         SoftLayer_SoapClient::setObjectMask()
+ * @see         SoftLayer_XmlrpcClient::setObjectMask()
  */
 class SoftLayer_ObjectMask
 {
--- XmlrpcClient.class.php
+++ XmlrpcClient.class.php
@@ -1,4 +1,31 @@
 <?php
+/**
+ * Copyright (c) 2009, SoftLayer Technologies, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither SoftLayer Technologies, Inc. nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 require_once dirname(__FILE__) . '/Common/ObjectMask.class.php';
 
@@ -104,14 +131,14 @@ if (version_compare(PHP_VERSION, '5', '<')) {
  * }
  *
  * The most up to date version of this library can be found on the SoftLayer
- * Development Network wiki: http://sldn.softlayer.com/wiki/ . Please post to
+ * github public repositories: http://github.com/softlayer/ . Please post to
  * the SoftLayer forums <http://forums.softlayer.com/> or open a support ticket
  * in the SoftLayer customer portal if you have any questions regarding use of
  * this library.
  *
  * @author      SoftLayer Technologies, Inc. <sldn@softlayer.com>
  * @copyright   Copyright (c) 2008, Softlayer Technologies, Inc
- * @license     http://creativecommons.org/licenses/by/3.0/us/ Creative Commons Attribution 3.0 US
+ * @license     http://sldn.softlayer.com/wiki/index.php/License
  * @link        http://sldn.softlayer.com/wiki/index.php/The_SoftLayer_API The SoftLayer API
  * @see         SoftLayer_XmlrpcClient_AsynchronousAction
  */
