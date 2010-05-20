75c3b49b7026bf5cf590d50c3e67a91ada4c7d6d
SoftLayer <sldn@softlayer.com>
2010-05-20 15:43:32 -0500

    Updated copyright statement year.

--- SoftLayer/XmlrpcClient.class.php
+++ SoftLayer/XmlrpcClient.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2009, SoftLayer Technologies, Inc. All rights reserved.
+ * Copyright (c) 2009 - 2010, SoftLayer Technologies, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
